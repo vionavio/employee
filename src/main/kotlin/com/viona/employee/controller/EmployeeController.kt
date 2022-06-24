94748bbaf747bb4fc677b8d0c93b8daae29cb613
Viona <viona.maryati@stockbit.com>
2022-06-24 13:30:02 +0700

    add employee

--- src/main/kotlin/com/viona/employee/controller/EmployeeController.kt
+++ src/main/kotlin/com/viona/employee/controller/EmployeeController.kt
@@ -1,6 +1,7 @@
 package com.viona.employee.controller
 
 import com.viona.employee.entity.Employee
+import com.viona.employee.request.EmployeeRequest
 import com.viona.employee.response.BaseResponse
 import com.viona.employee.service.EmployeeService
 import org.bson.types.ObjectId
@@ -16,16 +17,16 @@ class EmployeeController{
     @Autowired
     private lateinit var employeeService: EmployeeService
 
-//    @PostMapping
-//    fun createEmployee(@RequestBody request: EmployeeRequest): ResponseEntity<List<EmployeeResponse>> {
-//        val createdEmployee = employeeService.createEmployee(request)
-//
-//        return ResponseEntity.ok(
-//                EmployeeResponse(
-//
-//                )
-//            )
-//    }
+    @PostMapping
+    fun createEmployee(@RequestBody request: EmployeeRequest): BaseResponse<List<Employee>> {
+        val createdEmployee = employeeService.createEmployee(request)
+
+        return BaseResponse(
+            status = true,
+            message = "Success",
+            data = createdEmployee
+        )
+    }
 
     @GetMapping
     fun findAllEmployees(): BaseResponse<List<Employee>> {
